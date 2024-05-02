58e7113280c2c31fb79b5c66e15db8a5b03353c2
Patrick Barsanti <patrick.barsanti@amarulasolutions.com>
2024-05-02 15:25:32 +0200

    task-06: Add task 06
    Add task 06 of eudyptula challenge.
    The misc device registers/unregisters when module loads/unloads and can
    be found at `/dev/eudyptula`.
    Returns id when read from, and returns success value only when writing
    the correct id to it.
    Used `pat` as assigned id.
    
    Signed-off-by: Patrick Barsanti <patrick.barsanti@amarulasolutions.com>

---
+++ task-06/hello.c
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: GPL-2.0+
+
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/miscdevice.h>
+#include <linux/module.h>
+
+#define USERID	"pat\n"
+
+static ssize_t hello_misc_read(struct file *filp, char __user *buf,
+				size_t count, loff_t *ppos)
+{
+	return simple_read_from_buffer(buf, count, ppos,
+			USERID, strlen(USERID));
+}
+
+static ssize_t hello_misc_write(struct file *filp, const char __user *buf,
+				size_t count, loff_t *ppos)
+{
+	if (count != strlen(USERID))
+		return -EINVAL;
+
+	char str[strlen(USERID)];
+	int ret;
+
+	ret = simple_write_to_buffer(str, count, ppos, buf, count);
+
+	pr_debug("Device received: %s\n", str);
+
+	if (strncmp(str, USERID, count))
+		return -EINVAL;
+
+	return count;
+}
+
+static const struct file_operations hello_fops = {
+	.owner = THIS_MODULE,
+	.read = hello_misc_read,
+	.write = hello_misc_write,
+};
+
+struct miscdevice hello_misc = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "eudyptula",
+	.fops = &hello_fops,
+};
+
+static int __init hello_init(void)
+{
+	int ret;
+
+	ret = misc_register(&hello_misc);
+	if (ret)
+		return ret;
+
+	pr_debug("Hello, world!\n");
+
+	return ret;
+}
+
+static void __exit hello_exit(void)
+{
+	misc_deregister(&hello_misc);
+	pr_debug("Goodbye, cruel world.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL v2");
+
