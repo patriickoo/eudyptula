7b8ad40c3550ddcea4646e75cbd1df8b4fce0670
Patrick Barsanti <patrick.barsanti@amarulasolutions.com>
2024-05-06 13:54:46 +0200

    task-09: Add task 09
    Add task 09 of eudyptula challenge.
    This is exactly the same as task 08, but using sysfs instead of debugfs.
    The `id`, `jiffies` and `foo` files are moved to `sysfs/kernel/eudyptula/`.
    
    Signed-off-by: Patrick Barsanti <patrick.barsanti@amarulasolutions.com>

---
+++ task-09/hello.c
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: GPL-2.0+
+
+#include <linux/cleanup.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kobject.h>
+#include <linux/module.h>
+#include <linux/rwsem.h>
+#include <linux/sysfs.h>
+#include <linux/vmalloc.h>
+
+#define USERID	"pat\n"
+
+static DECLARE_RWSEM(foo_lock);
+
+static void *foo_data;
+
+static struct kobject *hello_kobj;
+
+static ssize_t id_show(struct kobject *kobj, struct kobj_attribute *attr,
+			char *buf)
+{
+	return sysfs_emit(buf, USERID);
+}
+
+static ssize_t id_store(struct kobject *kobj, struct kobj_attribute *attr,
+			const char *buf, size_t count)
+{
+	if (count != strlen(USERID))
+		return -EINVAL;
+
+	pr_debug("hello: /sys/kernel/debug/eudyptula/id: received %s", buf);
+
+	if (strncmp(buf, USERID, count))
+		return -EINVAL;
+
+	return count;
+}
+
+static ssize_t foo_show(struct kobject *kobj, struct kobj_attribute *attr,
+			char *buf)
+{
+	guard(rwsem_read)(&foo_lock);
+
+	return sysfs_emit(buf, foo_data);
+}
+
+static ssize_t foo_store(struct kobject *kobj, struct kobj_attribute *attr,
+			const char *buf, size_t count)
+{
+	guard(rwsem_write)(&foo_lock);
+	strcpy(foo_data, buf);
+	return count;
+}
+
+static ssize_t jiffies_show(struct kobject *kobj, struct kobj_attribute *attr,
+			char *buf)
+{
+	return sysfs_emit(buf, "%ld\n", jiffies);
+}
+
+static ssize_t jiffies_store(struct kobject *kobj, struct kobj_attribute *attr,
+			const char *buf, size_t count)
+{
+	return -EINVAL;
+}
+
+static struct kobj_attribute id_attr =
+	__ATTR(id, 0664, id_show, id_store);
+static struct kobj_attribute jiffies_attr =
+	__ATTR(jiffies, 0444, jiffies_show, jiffies_store);
+static struct kobj_attribute foo_attr =
+	__ATTR(foo, 0644, foo_show, foo_store);
+
+static struct attribute *attrs[] = {
+	&id_attr.attr,
+	&jiffies_attr.attr,
+	&foo_attr.attr,
+	NULL,
+};
+
+static struct attribute_group attr_group = {
+	.attrs = attrs,
+};
+
+static int __init hello_init(void)
+{
+	int ret;
+
+	foo_data = vmalloc(PAGE_SIZE);  /* max size is 1 page */
+	
+	hello_kobj = kobject_create_and_add("eudyptula", kernel_kobj);
+	if (!hello_kobj)
+		return -ENOMEM;
+
+	ret = sysfs_create_group(hello_kobj, &attr_group);
+	if (ret)
+		kobject_put(hello_kobj);
+
+	pr_debug("hello: Hello, world!\n");
+
+	return ret;
+}
+
+static void __exit hello_exit(void)
+{
+	vfree(foo_data);
+	kobject_put(hello_kobj);
+	pr_debug("hello: Goodbye, cruel world.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_AUTHOR("Patrick Barsanti <patrick.barsanti@amarulasolutions.com");
+MODULE_LICENSE("GPL v2");
+
